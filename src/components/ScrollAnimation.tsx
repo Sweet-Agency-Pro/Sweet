51ce8b5d022a764c73287f41f4446e902037eddb
ShiftedDevs <contact@shift-devs.com>
2025-11-30 22:40:41 +0100

    Updated ScrollAnimation.tsx

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -27,7 +27,7 @@ function ScrollAnimation() {
 
       // Animation triggers when user scrolls into STRATA section
       const animationStart = strataSectionTop;
-      const animationEnd = strataSectionTop + (viewportHeight * 0.6);
+      const animationEnd = strataSectionTop + (viewportHeight * 0.4);
 
       // Calculate scroll progress FIRST (before using it)
       let currentProgress = 0;
