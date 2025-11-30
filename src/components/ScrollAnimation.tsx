1aa3b5e88f68feb7d6c202bd142f608569e11264
ShiftedDevs <contact@shift-devs.com>
2025-11-30 21:54:23 +0100

    Updated ScrollAnimation.tsx

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -46,7 +46,7 @@ function ScrollAnimation() {
 
       // Now use the calculated progress for visibility decisions
       const shouldHideStrata = currentProgress >= 0.75;
-      const shouldShowNavbar = currentProgress >= 0.8;
+      const shouldShowNavbar = currentProgress >= 0.5;
 
       // Update STRATA visibility
       setStrataVisible(!shouldHideStrata);
